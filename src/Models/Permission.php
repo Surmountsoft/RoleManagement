00f2287b4d36440eca4a463d7bc389a0ed2e576c
Rahul Sharma <rahul.sharma@surmountsoft.in>
2021-09-01 09:52:26 +0530

    first commit

---
+++ src/Models/Permission.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace RSoftech\Role\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Permission extends Model
+{
+    use HasFactory;
+
+    public function roles() {
+        return $this->belongsToMany('RSoftech\Role\Models\Role');
+    }
+}
---
+++ src/lang/en/messages.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+	'are_you_sure' => 'Are you sure ?',
+	'you_are_going_to_action_this_attribute' => 'You are going to :action this :attribute',
+	'attribute_action_successfully' => ':attribute :action successfully',
+	'internal_server_error' => 'Internal server error',
+	'attribute_does_not_exists' => ':attribute does not exist',
+	'user_registered' => 'User Registered',
+];
